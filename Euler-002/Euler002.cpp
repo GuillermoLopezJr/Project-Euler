70d6445b75a19a241d75859279fddba495aaa66b
GuillermoLopezJr <guillermolopez6988@yahoo.com>
2015-10-13 23:52:33 -0500

    Add Euler-002

--- Euler-002/Euler002.cpp
+++ Euler-002/Euler002.cpp
@@ -1,9 +1,25 @@
+//ANSWER: 4613732
+
 #include <iostream>
 
 using namespace std;
 
 int main()
 {
-	cout << "hellow world" << endl;
+	int term1 = 1;
+	int term2 = 2;
+	int sum = 2;
+	while(term1 <4000000 || term2<4000000)
+	{
+		term1 = term1+term2;
+		if(term1 % 2 == 0)
+			sum += term1;
+		term2 = term1+term2;
+		if(term2 %2 == 0)
+			sum += term2;
+	}
+	cout << "sum " << sum << endl;
+	cin.get();
+
 	return 0;
 }
