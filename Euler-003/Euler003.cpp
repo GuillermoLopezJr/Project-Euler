e7820a42a379ea2fb342d4488476d82f2ab24423
GuillermoLopezJr <guillermolopez6988@yahoo.com>
2015-10-16 18:22:44 -0500

    Add Euler-003

--- Euler-003/Euler003.cpp
+++ Euler-003/Euler003.cpp
@@ -1,9 +1,26 @@
+//ANSWER: 6857
 #include <iostream>
-
 using namespace std;
+
+//This function returns largest prime factor
+int get_factor(long long int n)
+{
+	int factor = 2;
+	while(n > 1)
+	{
+		while(n % factor == 0)
+			n /= factor;
+		++factor;
+	}
+	return --factor;
+}
 
 int main()
 {
-
-	cout << "hello world" << endl;
+	long long int num = 600851475143;
+	int maxPFactor = get_factor(num);
+	cout << "Max Prime Factor: " << maxPFactor << endl;
+	cin.get();
+	
+	return 0;
 }
